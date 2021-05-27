2e0535a721b86c940d61f40ea218439bb1bf77eb
SaraSanchezz <sarasanrodrigo@gmail.com>
2021-05-27 15:23:46 +0000

    Added sizes to product model and  its selector in product_details template

--- products/models.py
+++ products/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     size = models.TextField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
