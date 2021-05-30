fc770576a4dccf87a65bda55fd54d1cd7011e118
SaraSanchezz <sarasanrodrigo@gmail.com>
2021-05-30 12:03:35 +0000

    Added stripe basics

--- checkout/views.py
+++ checkout/views.py
@@ -16,5 +16,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key':'pk_test_51IwmiWIhtQbRiixnzNtNnD0Sbyt39jmwuQ0XBd1zYRStP2I2Kr1GGeDwV4QW6SH6pF8jHduqbF263v8HBWXILK5700okT38ug7',
+        'client_secret': 'test_client_secret',
     }
     return render(request, template, context)
