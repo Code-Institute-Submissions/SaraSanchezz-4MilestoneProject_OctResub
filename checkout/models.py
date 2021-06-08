bd87d08b0a49fdcd61a6a547013f3904c8b365e3
SaraSanchezz <sarasanrodrigo@gmail.com>
2021-06-08 17:50:32 +0000

    Added dropdown for countries selector in checkout form

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 from products.models import Product
+from django_countries.fields import CountryField
 
 
 # Create your models here.
@@ -16,7 +17,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
