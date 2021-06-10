1c2d19dc18ad5d5812053cc4f10e1940159bf5d3
SaraSanchezz <sarasanrodrigo@gmail.com>
2021-06-10 01:09:06 +0000

    Set DEBUG and removed SECRET_KEY from settings.py

--- brendas_shop/settings.py
+++ brendas_shop/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uves4k4+0qdll9_w+1^eifk&ns^9nk^sr^6kmaay&7gl@7jqhr'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['brendas-shop.herokuapp.com', 'localhost']
 
