0ff9b94faf020be22afa6cb5a483295cd919264a
SaraSanchezz <sarasanrodrigo@gmail.com>
2021-10-23 23:03:07 +0000

    Updated reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Post(models.Model):
+class Review(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False)
     rate = models.CharField(max_length=10, null=False, blank=False)
     name = models.CharField(max_length=50, null=False, blank=False)
