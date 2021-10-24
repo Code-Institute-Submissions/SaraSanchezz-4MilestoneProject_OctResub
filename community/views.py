aecf92e312876290bab3993cbce0e6a02f774b1f
SaraSanchezz <sarasanrodrigo@gmail.com>
2021-10-24 09:51:14 +0000

    Added post function to the vews of community app and updated src img  error for index.html

--- community/views.py
+++ community/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 
 def view_community(request):
     """ A view to show community page """
+    posts = Post.objects.all()
 
-    return render(request, 'community/community.html')
+    return render(request, 'community/community.html', {'posts': posts})
